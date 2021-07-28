b28a5f65955b4e17309c21343b6e1c64da050c54
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-28 12:01:49 +0200

    Make an if form which converts €, $, £ to RSD

--- index.php
+++ index.php
@@ -2,21 +2,40 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Currency converter</title>
 </head>
 <body>
 <form action="" method="get">
-    <label for="from">From:</label>
-    <input type="text" id="from" name="sourceCurrency">
-    <label for="to">To:</label>
-    <input type="text" id="to" name="goalCurrency">
-    <input type="submit" value="Convert">
+    <label for="sourceCurrency">From:</label>
+    <select name="sourceCurrency" id="sourceCurrency">
+        <option value="pounds">UK Pounds</option>
+        <option value="dollars">US Dollars</option>
+        <option value="euros">Euros</option>
+    </select>
+    <input type="number" name="input">
+    <input type="submit" name="submit" value="To Serbian Dinars">
 </form>
 </body>
 </html>
 
 <?php
+if (isset($_GET['submit'])) {
+    $input = $_GET['input'];
+    $sourceCurrency = $_GET['sourceCurrency'];
 
+    if ($sourceCurrency == 'euros') {
+        $output = $input * 117.49;
+        echo number_format($output, 2) . 'RSD';
+    }
+    else if ($sourceCurrency == 'dollars'){
+        $output = $input * 99.51;
+        echo number_format($output, 2) . 'RSD';
+    }
+    else if ($sourceCurrency == 'pounds'){
+        $output = $input * 138.08;
+        echo number_format($output, 2) . 'RSD';
+    }
+}
 ?>
