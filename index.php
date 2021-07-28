662a5c73735e10723e781cbd6bab9fc310bd792f
Tanja <tanjamilisavljevic7@gmail.com>
2021-07-28 09:32:50 +0200

    Create an html form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Currency converter</title>
+</head>
+<body>
+<form action="" method="get">
+    <label for="from">From:</label>
+    <input type="text" id="from" name="sourceCurrency">
+    <label for="to">To:</label>
+    <input type="text" id="to" name="goalCurrency">
+    <input type="submit" value="Convert">
+</form>
+</body>
+</html>
+
+<?php
+
+?>
